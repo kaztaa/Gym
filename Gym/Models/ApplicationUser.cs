5eaeaa8e8a18057f74fc76b0fdc15abd0976508e
dzta <dzta@protonmail.com>
2024-11-15 13:05:14 +0100

    Logout return to GymClass. Övning 4 done

--- Gym/Models/ApplicationUser.cs
+++ Gym/Models/ApplicationUser.cs
@@ -7,7 +7,7 @@ namespace Gym.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName => @"{FirstName} {LastName}";
+        public string FullName => $"{FirstName} {LastName}";
         public DateTime TimeOfRegistration { get; set; }
         public ICollection<ApplicationUserGymClass> AttendedClasses { get; set; }
     }
